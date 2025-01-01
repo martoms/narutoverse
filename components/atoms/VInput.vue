ac1ab2ac5f15babfedff83f02c6a2a8432332753
martomz <tomataomarjohn@gmail.com>
2025-01-01 20:10:22 +0800

    Create VInput

---
+++ components/atoms/VInput.vue
@@ -0,0 +1,16 @@
+<template>
+  <input :type="type" :id="id" :name="name" :placeholder="placeholder" v-model.trim="model" />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: 'text' | 'password'
+  id: string
+  name: string
+  placeholder?: string
+}
+
+defineProps<Props>()
+
+const model = defineModel<string | number>()
+</script>
