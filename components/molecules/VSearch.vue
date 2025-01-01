ba3bb3fd5c5a78dc0ed4c08e9b4d8843a205418c
martomz <tomataomarjohn@gmail.com>
2025-01-01 20:17:15 +0800

    Create VSearch

---
+++ components/molecules/VSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="relative flex items-center gap-2">
+    <AtomsVInput
+      id="search"
+      name="search"
+      placeholder="Search"
+      class="w-full h-full rounded-full p-1 pb-2 pl-7 text-light bg-stone-800 focus:outline focus:outline-primary"
+    />
+    <AtomsVIcon name="icon-search" class="absolute left-2 text-primary" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
