363e11ce66a6b6d3d115beb5c8482509c9d0e7bd
martomz <tomataomarjohn@gmail.com>
2025-01-01 20:02:54 +0800

    Create VIcon

---
+++ components/atoms/VIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <i class="v-icon" :class="[name, getSizes]"></i>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  name: string
+  size?: 'xs' | 'sm' | 'lg' | 'xl'
+}
+
+const { size = 'md' } = defineProps<Props>()
+
+const getSizes = computed(() => (size === 'md' ? 'text-base' : 'text-' + size))
+</script>
+
+<style>
+@import url('/assets/css/icons.css');
+</style>
