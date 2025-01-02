d9309f1954512b115b127fdff5a2118f4896fa9f
martomz <tomataomarjohn@gmail.com>
2025-01-02 15:36:13 +0800

    Modify PersonalSchema

--- schemas/category.ts
+++ schemas/category.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z } from 'zod'
 
 export const CategoriesSchema = z.enum([
   'Characters',
@@ -8,7 +8,7 @@ export const CategoriesSchema = z.enum([
   'Tailed Beasts',
   'Teams',
   'Villages',
-  'Akatsuki'
+  'Akatsuki',
 ])
 
 export const ImagesSchema = z.array(z.string().url())
@@ -17,18 +17,22 @@ export const FamilySchema = z.record(z.string(), z.any()).optional()
 export const JutsuSchema = z.array(z.string()).optional()
 export const NatureTypeSchema = z.array(z.string()).optional()
 export const UniqueTraitsSchema = z.array(z.string()).optional()
-export const VoiceActorsSchema =  z.record(z.string(), z.union([z.string(), z.array(z.string())])).optional()
+export const VoiceActorsSchema = z
+  .record(z.string(), z.union([z.string(), z.array(z.string())]))
+  .optional()
 export const ToolsSchema = z.array(z.string()).optional()
 export const RankSchema = z.any().optional()
-export const PersonalSchema = z.record(z.string(), z.any()).optional()
+export const PersonalSchema = z
+  .union([z.record(z.string(), z.any()), z.array(z.string())])
+  .optional()
 
 export const BasicCategorychema = z.object({
   id: z.number(),
-  name: z.string()
+  name: z.string(),
 })
 export const BasicDataSchema = z.object({
   currentPage: z.number(),
-  pageSize: z.number()
+  pageSize: z.number(),
 })
 export const CharacterSchema = BasicCategorychema.extend({
   images: ImagesSchema,
@@ -38,57 +42,59 @@ export const CharacterSchema = BasicCategorychema.extend({
   jutsu: JutsuSchema,
   natureType: NatureTypeSchema,
   uniqueTraits: UniqueTraitsSchema,
-  voiceActors: VoiceActorsSchema
+  voiceActors: VoiceActorsSchema,
 })
 export const ClanSchema = BasicCategorychema.extend({
   image: z.array(z.string().url()).optional(),
-  characters: z.array(CharacterSchema)
+  characters: z.array(CharacterSchema),
 })
 export const KaraSchema = CharacterSchema.extend({
   tools: ToolsSchema,
-  rank: RankSchema
+  rank: RankSchema,
 })
 export const KekkeiGenkaiSchema = BasicCategorychema.extend({
-  characters: z.array(CharacterSchema)
+  characters: z.array(CharacterSchema),
 })
 export const TailedBeastSchema = CharacterSchema.extend({
-  voiceActors: VoiceActorsSchema
+  voiceActors: VoiceActorsSchema,
 })
 export const TeamsSchema = KekkeiGenkaiSchema
 export const VillagesSchema = KekkeiGenkaiSchema
 export const AkatsukiSchema = CharacterSchema
 
-export const DataSchema = z.union([
-  BasicDataSchema.extend({
-    totalCharacters: z.number(),
-    characters: z.array(CharacterSchema)
-  }),
-  BasicDataSchema.extend({
-    totalClans: z.number(),
-    clans: z.array(ClanSchema)
-  }),
-  BasicDataSchema.extend({
-    totalKara: z.number(),
-    kara: z.array(KaraSchema)
-  }),
-  BasicDataSchema.extend({
-    totalKekkeiGenkai: z.number(),
-    kekkeigenkai: z.array(KekkeiGenkaiSchema)
-  }),
-  BasicDataSchema.extend({
-    totalTailedBeasts: z.number(),
-    tailedBeasts: z.array(TailedBeastSchema)
-  }),
-  BasicDataSchema.extend({
-    totalTeams: z.number(),
-    teams: z.array(TeamsSchema)
-  }),
-  BasicDataSchema.extend({
-    totalVillages: z.number(),
-    villages: z.array(VillagesSchema)
-  }),
-  BasicDataSchema.extend({
-    totalMembers: z.number(),
-    akatsuki: z.array(AkatsukiSchema)
-  })
-]).nullish()
+export const DataSchema = z
+  .union([
+    BasicDataSchema.extend({
+      totalCharacters: z.number(),
+      characters: z.array(CharacterSchema),
+    }),
+    BasicDataSchema.extend({
+      totalClans: z.number(),
+      clans: z.array(ClanSchema),
+    }),
+    BasicDataSchema.extend({
+      totalKara: z.number(),
+      kara: z.array(KaraSchema),
+    }),
+    BasicDataSchema.extend({
+      totalKekkeiGenkai: z.number(),
+      kekkeigenkai: z.array(KekkeiGenkaiSchema),
+    }),
+    BasicDataSchema.extend({
+      totalTailedBeasts: z.number(),
+      tailedBeasts: z.array(TailedBeastSchema),
+    }),
+    BasicDataSchema.extend({
+      totalTeams: z.number(),
+      teams: z.array(TeamsSchema),
+    }),
+    BasicDataSchema.extend({
+      totalVillages: z.number(),
+      villages: z.array(VillagesSchema),
+    }),
+    BasicDataSchema.extend({
+      totalMembers: z.number(),
+      akatsuki: z.array(AkatsukiSchema),
+    }),
+  ])
+  .nullish()
