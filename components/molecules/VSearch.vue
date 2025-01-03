7ea388733f3dbe301ec53596f13a17feda3d071e
martomz <tomataomarjohn@gmail.com>
2025-01-03 08:02:05 +0800

    Modify VSearch

--- components/molecules/VSearch.vue
+++ components/molecules/VSearch.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="relative flex items-center gap-2">
-    <form @submit.prevent="emits('submit')">
-      <AtomsVInput
-        id="search"
-        name="search"
-        placeholder="Complete Name"
-        v-model="model"
-        class="w-full h-full rounded-full p-1 pb-2 pl-7 text-light bg-stone-800 focus:outline focus:outline-primary"
-      />
-    </form>
+    <AtomsVInput
+      id="search"
+      name="search"
+      :placeholder
+      v-model="model"
+      class="w-full h-full box-border pl-8 text-sm text-light bg-stone-800 focus:outline-none"
+      @focus="emits('focus')"
+      @blur="emits('blur')"
+    />
     <AtomsVIcon name="icon-search" class="absolute left-2 text-primary" />
   </div>
 </template>
 
 <script setup lang="ts">
+type Props = { placeholder?: string }
+
+defineProps<Props>()
+
 const model = defineModel<string>()
-const emits = defineEmits(['submit'])
+const emits = defineEmits(['submit', 'focus', 'blur'])
 </script>
